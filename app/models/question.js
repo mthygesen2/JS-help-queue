81f1e172af7aa6baf66011e8196d95238bf4dcde
Mao Thygesen <maothygesen@gmail.com>
2016-04-04 09:09:47 -0700

    add teacher route with new question component

--- app/models/question.js
+++ app/models/question.js
@@ -3,5 +3,5 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   studentName: DS.attr(),
   location: DS.attr(),
-  question: DS.attr()
+  content: DS.attr()
 });
--- config/environment.js
+++ config/environment.js
@@ -5,7 +5,7 @@ module.exports = function(environment) {
     modulePrefix: 'help-queue',
     environment: environment,
     contentSecurityPolicy: { 'connect-src': "'self' https://auth.firebase.com wss://*.firebaseio.com" },
-    firebase: 'https://YOUR-FIREBASE-NAME.firebaseio.com/',
+    firebase: 'https://js-ember-help-queue.firebaseio.com/',
     baseURL: '/',
     locationType: 'auto',
     EmberENV: {
